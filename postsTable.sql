b4c2f0beda1e26767ab30da75a01687c8cbae364
RTanjutcoII <rtanjutcoii@fordham.edu>
2024-11-20 23:44:21 -0500

    Add files via upload

---
+++ postsTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `posts`
+   (
+      `id`   integer(11) NOT NULL AUTO_INCREMENT,
+      `title` varchar(80) NOT NULL,
+      `content` varchar(255) NOT NULL,
+      primary key (`id`)
+   );
